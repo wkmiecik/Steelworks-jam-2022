c2ee887cf300916e0735d712d26850d9e1c35550
LordPapkin <papkin2013@gmail.com>
2022-10-29 01:07:42 +0200

    Tweaks for Movement, added Jumps

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float groundDrag;
+    [Header("Jump")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMultiplayer;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
 
     [SerializeField] private Transform orientation;
+    [SerializeField] private TextMeshProUGUI speedText;
+
 
     private float horizontalInput;
     private float verticalInput;
     private Vector3 moveDirection;
+
+    private bool isGrounded;
+    private bool isReadyToJump = true;
+
     private Rigidbody rb;
 
     private void Awake()
@@ -22,23 +39,82 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        MyInput();
+        GroundCheck();
+        GetKeyboardInput();
+        //SpeedControl();
+        ApplyDrag();        
     }
+    
     private void FixedUpdate()
     {
         MovePlayer();
     }
 
-    private void MyInput()
+    private void GetKeyboardInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(jumpKey) && isReadyToJump && isGrounded)
+        {
+            isReadyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         //get walk direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplayer, ForceMode.Force);
+        }
+        
     }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x * airMultiplayer, 0f, rb.velocity.z * airMultiplayer);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        isReadyToJump = true;
+    }
+
+    private void ApplyDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0f;
+        }
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+    }
+
+    //private void SpeedControl()
+    //{
+    //    Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+    //    if(flatVel.magnitude > moveSpeed)
+    //    {
+    //        Vector3 limitedVel = flatVel.normalized * moveSpeed;
+    //        rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);           
+    //    }        
+    //}
 }
