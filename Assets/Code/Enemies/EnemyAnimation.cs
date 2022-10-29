684188c620df1d39d423394a1b9ac07c783203d2
Waldemar Kmiecik <waldemar.kmiecik@microsoft.wsei.edu.pl>
2022-10-29 06:21:47 +0200

    added dotween

---
+++ Assets/Code/Enemies/EnemyAnimation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
