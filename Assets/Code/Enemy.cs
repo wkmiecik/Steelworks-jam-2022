ba2ee92cf94fb0f9e959d4e0149104a02ea51230
Waldemar Kmiecik <waldemar.kmiecik@microsoft.wsei.edu.pl>
2022-10-29 06:03:29 +0200

    Enemy following player

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float speed;
+
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(GameManager.instance.PlayerCameraPos);
+    }
+
     public void Die()
     {
         StartCoroutine("DyingCoroutine");
