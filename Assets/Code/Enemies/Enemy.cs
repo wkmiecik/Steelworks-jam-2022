e1d3b22c18f9cfd7cfa5cc2f7f9da2b7d3b6e202
Waldemar Kmiecik <waldemar.kmiecik@microsoft.wsei.edu.pl>
2022-10-30 12:31:40 +0100

    Added enemy dying animation

--- Assets/Code/Enemies/Enemy.cs
+++ Assets/Code/Enemies/Enemy.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
+using UnityEngine.VFX;
 
 public class Enemy : MonoBehaviour
 {
     NavMeshAgent agent;
+    EnemyAnimation model;
     BoxCollider damageTrigger;
     PlayerMovement pm;
+    VisualEffect vfx;
+    MeshRenderer meshRenderer;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        model = GetComponentInChildren<EnemyAnimation>();
         damageTrigger = GetComponent<BoxCollider>();
         pm = FindObjectOfType<PlayerMovement>();
+        vfx = GetComponentInChildren<VisualEffect>();
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
     }
 
     void Update()
@@ -22,20 +30,21 @@ public class Enemy : MonoBehaviour
             agent.SetDestination(pm.transform.position);
     }
 
-    public void Die()
+    public void Die(Vector3 impulse)
     {
-        StartCoroutine("DyingCoroutine");
-    }
-
-    IEnumerator DyingCoroutine()
-    {
-        // todo: dying animation
+        model.tween.Kill();
         agent.enabled = false;
         damageTrigger.enabled = false;
+        
+        var force = impulse / Time.fixedDeltaTime;
 
-        yield return new WaitForSeconds(1);
-        Destroy(gameObject);
+        model.transform.DOBlendableLocalRotateBy(new Vector3(0, 0, 90), 1f);
+        model.transform.DOBlendableMoveBy(Vector3.down * 2f, 1f);
+        //model.transform.DOBlendableMoveBy(Vector3.forward * -1f, 1f);
+
+        StartCoroutine(Dying());
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -43,5 +52,22 @@ public class Enemy : MonoBehaviour
             var healthSystem = other.GetComponentInParent<healthSystem>();
             healthSystem.TakeDamege(20);
         }
+    }
+
+    private IEnumerator Dying()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        vfx.Play();
+
+        yield return new WaitForSeconds(1);
+
+        meshRenderer.enabled = false;
+
+        yield return new WaitForSeconds(2);
+
+        Destroy(gameObject);
+
+        yield return 0;
     }
 }
